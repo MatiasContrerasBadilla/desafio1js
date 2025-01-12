4cdd110cda74d92b77ee953e047a5de516b07722
Matías <contrerasbadilla.matias@gmail.com>
2025-01-12 08:43:50 -0300

    otra vez

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-precio = 400000
-
-precioSpan = document.querySelector(".precio-inicial");
+let precio = 400000
+let precioSpan = document.querySelector(".precio-inicial");
+let cantidadSpan = document.querySelector(".cantidad")
+let valorTotalSpan = document.querySelector(".valor-total")
+let cantidad = 0
 precioSpan.innerHTML = precio
+
+
+
+document.getElementById("botonMas").onclick = function(){
+    cantidad += 1	
+    cantidadSpan.innerHTML = cantidad 
+    valorTotalSpan.innerHTML = precio * cantidad
+
+} 
+document.getElementById("botonMenos").onclick = function(){
+   if (cantidad > 0) {
+    cantidad -= 1}
+   cantidadSpan.innerHTML = cantidad
+   valorTotalSpan.innerHTML = precio * cantidad
+} 
+
